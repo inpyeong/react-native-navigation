f4e3f7d76af6b8eac94951cb7377fab74b941d88
inpyeong <jinipyung@gmail.com>
2020-10-10 20:48:06 +0900

    Apply screenOptions property of Stack.Navigator

--- App.js
+++ App.js
@@ -7,56 +7,67 @@
  */
 
 import 'react-native-gesture-handler';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  StyleSheet,
-  View,
-  Text,
+    StyleSheet,
+    View,
+    Text,
 } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './src/home';
 import UserScreen from './src/user';
 
 const Stack = createStackNavigator();
 
 class App extends Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{title: 'Home Screen'}}
-          />
-          <Stack.Screen
-              name="User"
-              component={UserScreen}
-              initialParams={{
-                userIdx: 50,
-                userName: 'inpyung',
-                userLastName: 'Lee',
-              }}
-              // options={{
-              //   title: 'User Screen',
-              //   headerStyle: {
-              //     backgroundColor: 'pink',
-              //   },
-              //   headerTintColor: 'red',
-              //   headerTitleStyle: {
-              //     fontWeight: 'bold',
-              //     color: 'purple'
-              //   }
-              // }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
-  }
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Home"
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: '#a4511e',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                            color: '#f3d612',
+                        },
+                    }}
+                >
+                    <Stack.Screen
+                        name="Home"
+                        component={HomeScreen}
+                        options={{title: 'Home Screen'}}
+                    />
+                    <Stack.Screen
+                        name="User"
+                        component={UserScreen}
+                        initialParams={{
+                            userIdx: 50,
+                            userName: 'inpyung',
+                            userLastName: 'Lee',
+                        }}
+                        options={{
+                          title: 'User Screen',
+                          headerStyle: {
+                            backgroundColor: 'pink',
+                          },
+                          headerTintColor: 'red',
+                          headerTitleStyle: {
+                            fontWeight: 'bold',
+                            color: 'purple'
+                          }
+                        }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 export default App;
