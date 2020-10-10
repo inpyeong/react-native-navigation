f34061422f34b209048f55fb35325e4a0e66c00b
inpyeong <jinipyung@gmail.com>
2020-10-10 17:51:22 +0900

    Apply nagivation

---
+++ src/home.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+class HomeScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default HomeScreen;
---
+++ src/user.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+class UserScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>User Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default UserScreen;
